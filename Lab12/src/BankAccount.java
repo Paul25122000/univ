9098952a40ea13322f3e454b94c5f7ab5c025d50
tonualexandru <tonu.alexandru99@gmail.com>
2020-05-29 04:59:29 +0300

    artifact added

--- Lab12/src/BankAccount.java
+++ Lab12/src/BankAccount.java
@@ -2,13 +2,12 @@ public class BankAccount {
 
     private int amount;
     private Boolean available = false;
-    private static final int limit = 1000;
 
     public BankAccount(int amount) {
         this.amount = amount;
     }
 
-    public synchronized int withdraw(int amount, int transactionNumber) {
+    public synchronized void withdraw(int amount, int transactionNumber) {
         if (!available) {
             try {
                 wait();
@@ -23,7 +22,6 @@ public class BankAccount {
         }
         available = false;
         notify();
-        return this.amount;
     }
 
     public synchronized void deposit(int amount, int transactionNumber) {
@@ -34,9 +32,7 @@ public class BankAccount {
                 e.printStackTrace();
             }
         }
-        if(this.amount + amount <= limit) {
-            this.amount += amount;
-        }
+        this.amount += amount;
         available = true;
         System.out.printf("+ Transaction %d: %4dRON has been deposited, %4dRON left%n",
                 transactionNumber, amount, this.amount);
