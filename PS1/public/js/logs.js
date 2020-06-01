e0db2e73bf8ab669b31845bc85e760ece373a690
tonualexandru <tonu.alexandru99@gmail.com>
2020-06-01 22:34:48 +0300

    feat: filter logs and clear filter

--- PS1/public/js/logs.js
+++ PS1/public/js/logs.js
@@ -10,6 +10,7 @@ const api = 'http://192.168.64.6/public/api/logs/';
 
 var logs = []
 var lastLogId;
+var filter = undefined;
 
 async function getRecordsAsync(url) {
     let response = await fetch(url),
@@ -18,7 +19,7 @@ async function getRecordsAsync(url) {
 }
 
 const updateLogsCollection = (data, container, type, isOldLog) => {
-    if (!isOldLog)
+    if (!isOldLog && lastLogId != data?.id)
         logs.push(data)
     let row =
         `<tr class="log">
@@ -44,8 +45,10 @@ const updateLogsCollection = (data, container, type, isOldLog) => {
     row += `
         <td><i class="material-icons clickable" onclick="deleteLog('${data.id}')">delete</i><td>
         </tr>`
-    container.innerHTML += row;
-    lastLogId = data.id
+    if (filter === undefined || new Date(data.timestamp) >= filter)
+        container.innerHTML += row;
+    if (data?.id > lastLogId || lastLogId == undefined)
+        lastLogId = data?.id
 }
 
 const updateRoutine = (container, type, autoReload) => {
@@ -62,55 +65,38 @@ const updateRoutine = (container, type, autoReload) => {
                                 let firstLog = container.querySelector(".log");
                                 container.removeChild(firstLog);
                             }
-                            updateLogsCollection(log, container, type)
+                            updateLogsCollection(log, container, type, false)
                         }
                     })
                 })
         }
-        console.log(autoReload.checked)
     }, 3000)
 }
 
 document.addEventListener('DOMContentLoaded', function () {
     const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-    const current_datetime = new Date();
-    const formattedDate = months[current_datetime.getMonth()] + ' ' + current_datetime.getDate() + ", " + current_datetime.getFullYear();
+    const currentDatetime = new Date();
+    const formattedDate = months[currentDatetime.getMonth()] + ' ' + currentDatetime.getDate() + ", " + currentDatetime.getFullYear();
 
 
     const logsContainer = document.querySelector("#logs tbody");
     const renderLogs = (records) => {
         records.forEach(element => {
-            updateLogsCollection(element, logsContainer)
+            updateLogsCollection(element, logsContainer, null, false)
         });
     }
 
     const getActiveTab = () => {
         var tab = document.querySelector('.tabs .active');
-        return tab.innerHTML;
+        return tab.innerHTML.toLowerCase();
     }
 
     const onTabChange = (tabName) => {
         logsContainer.innerHTML = '';
         logs.forEach(log => {
-            updateLogsCollection(log, tableBody, tabName, true)
+            updateLogsCollection(log, logsContainer, tabName.toLowerCase(), true)
         });
     }
-
-    const deleteAll = document.querySelector("#deleteAll");
-    deleteAll.onclick = async () => {
-        try {
-            const response = await
-                fetch(api + "?deleteAll=1", { method: "DELETE" })
-            if (response.ok) {
-                const text = await response.json();
-                M.toast({ html: text })
-            }
-        } catch (err) {
-            M.toast({ html: 'Something went wrong' })
-            console.error(err)
-        }
-    }
-
 
     const autoReloadSwitch = document.querySelector(".switch input");
     getRecordsAsync(api)
@@ -126,15 +112,51 @@ document.addEventListener('DOMContentLoaded', function () {
         })
         logsContainer.removeChild(nearest("log", event.target));
     }
+
+    const deleteAll = document.querySelector("#deleteAll");
+    deleteAll.onclick = async () => {
+        try {
+            const response = await
+                fetch(api + "?deleteAll=1", { method: "DELETE" })
+            if (response.ok) {
+                const text = await response.json();
+                logsContainer.innerHTML = '';
+                M.toast({ html: text, classes: 'indigo_toast' })
+            }
+        } catch (err) {
+            M.toast({ html: 'Something went wrong', classes: 'indigo_toast' })
+            console.error(err)
+        }
+    }
+
     setFilter = () => {
         let date = this.querySelector(".datepicker").value,
             time = this.querySelector(".timepicker").value;
         if (date != "" && time != "") {
-            // updateLogs(getActiveTab().toLowerCase(), date, time + ":00")
+            autoReloadSwitch.checked = false;
+            logsContainer.innerHTML = '';
+            filter = new Date(date + " " + time);
+            const type = getActiveTab()
+            logs.forEach(log => {
+                updateLogsCollection(log, logsContainer, type, true)
+            });
         }
         else {
-            M.toast({ html: 'Please select date and time' })
+            M.Datepicker.getInstance(datepicker[0]).open();
+            M.toast({ html: 'Please select date and time', classes: 'indigo_toast' })
         }
+    }
+
+    const clearFilter = document.querySelector("#clearFilter")
+    clearFilter.onclick = () => {
+        filter = undefined;
+        autoReloadSwitch.checked = true
+        logsContainer.innerHTML = ''
+        const type = getActiveTab()
+        console.log(type)
+        logs.forEach(log => {
+            updateLogsCollection(log, logsContainer, type, true)
+        })
     }
 
     const navElems = document.querySelectorAll('.sidenav'),
@@ -154,14 +176,18 @@ document.addEventListener('DOMContentLoaded', function () {
         currentYear = currentDate.getFullYear(),
         dateOptions = {
             "maxDate": currentDate,
-            "yearRange": [currentYear - 5, currentYear]
+            "yearRange": [currentYear - 5, currentYear],
+            "onClose": () => { M.Timepicker.getInstance(timepicker[0]).open() }
         },
         timeOptions = {
             "twelveHour": false
-        };
+        },
+        modlas = document.querySelectorAll('.modal');
 
     M.Sidenav.init(navElems, navOptions);
     M.Datepicker.init(datepicker, dateOptions);
     M.Timepicker.init(timepicker, timeOptions);
     M.Tabs.init(tabElems, tabOptions);
+    M.Modal.init(modlas);
+
 });
