6663a661c0438bf6ee06f5a81c6511f4653eae55
tonualexandru <tonu.alexandru99@gmail.com>
2020-06-02 23:12:29 +0300

    add circles one by one

--- PJ/Coroi/src/Main.java
+++ PJ/Coroi/src/Main.java
@@ -1,11 +1,10 @@
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Main extends JFrame {
@@ -17,29 +16,11 @@ public class Main extends JFrame {
     private JComboBox comboBox1;
     private JButton btnWrite;
     private JPanel drawingPane;
-    private JPanel templatePane;
 
     private int counter = 1;
     private int radius = 20;
 
-    class CircleDrawer extends JPanel {
-        private static final long serialVersionUID = 1L;
-        private int x;
-        private int y;
-        private int radius;
-        CircleDrawer(int x, int y, int radius) {
-            this.x = x;
-            this.y = y;
-            this. radius = radius;
-            setPreferredSize(new Dimension(radius, radius));
-        }
-        @Override
-        public void paintComponent(Graphics g) {
-            super.paintComponent(g);
-            g.fillOval(x, y, radius, radius);
-        }
-    }
-
+    List<Integer> circles = new ArrayList<Integer>();
 
     public Main(String title) {
         super(title);
@@ -51,11 +32,11 @@ public class Main extends JFrame {
 
         btnAdd.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                System.out.println("add");
-                if(counter == 10) {
+                if (counter == 10) {
                     msg.setText("Can not increment");
-                }
-                else {
+                } else {
+                    circles.add(radius);
+                    drawCircles();
                     msg.setText("");
                     count.setText(String.valueOf(++counter));
                 }
@@ -63,25 +44,27 @@ public class Main extends JFrame {
         });
         btnRemove.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                System.out.println("remove");
-                if(counter == 1) {
+                if (counter == 1) {
                     msg.setText("Can not decrement");
-                }
-                else {
+                } else {
+                    drawingPane.repaint();
+                    circles.remove(counter - 1);
+                    drawCircles();
                     msg.setText("");
                     count.setText(String.valueOf(--counter));
                 }
             }
         });
 
-        Random ran = new Random();
-        int radius = ran.nextInt(26);
-        if(radius < 20)
-            radius = 20 + radius % 20;
+        drawingPane.setSize(600, 200);
 
-        this.radius = radius;
-        comboBox1.addItem(25);
+        Random ran = new Random();
+        radius = ran.nextInt(26);
+        if (radius < 20 || radius > 24)
+            radius = 20 + radius % 4;
+
         comboBox1.addItem(this.radius);
+        comboBox1.addItem(25);
 
         btnWrite.addActionListener(new ActionListener() {
             @Override
@@ -97,10 +80,14 @@ public class Main extends JFrame {
             }
         });
 
-        comboBox1.addPropertyChangeListener(new PropertyChangeListener() {
+        circles.add(radius);
+        drawCircles();
+
+        comboBox1.addActionListener(new ActionListener() {
             @Override
-            public void propertyChange(PropertyChangeEvent evt) {
-                setRadius((Integer) comboBox1.getSelectedItem());
+            public void actionPerformed(ActionEvent e) {
+                radius = (int) comboBox1.getSelectedItem();
+                System.out.println(radius);
             }
         });
     }
@@ -108,19 +95,22 @@ public class Main extends JFrame {
     private void setRadius(int radius) {
         this.radius = radius;
     }
+
     private int getRadius() {
         return this.radius;
     }
 
+    private void drawCircles() {
+        System.out.println(radius);
+        for (int i = 0; i < circles.size(); i++) {
+            drawingPane.getGraphics().drawOval(i * 30, 0, circles.get(i), circles.get(i));
+        }
+    }
 
     public static void main(String[] args) {
         JFrame frame = new Main("Circles");
         frame.setVisible(true);
-        frame.setSize(400, 300);
-    }
-
-    private void createUIComponents() {
-        templatePane = new CircleDrawer(0, 0, radius);
+        frame.setSize(600, 300);
     }
 }
 
